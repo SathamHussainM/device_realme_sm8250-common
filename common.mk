b12b355e16a21726f563a526e2e5707248e37ab5
LuK1337 <priv.luk@gmail.com>
2022-04-14 17:25:48 +0200

    sm8250-common: Set DSDS based on simcardnum.doublesim=1
    Change-Id: Ib0ae4728b33aefeaf50f0c3c074481ef35ccb0b0

--- common.mk
+++ common.mk
@@ -241,6 +241,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom.ramdisk \
     init.class_main.sh \
     init.oplus.rc \
+    init.oplus.sh \
     init.qcom.early_boot.sh \
     init.qcom.post_boot.sh \
     init.qcom.rc \
