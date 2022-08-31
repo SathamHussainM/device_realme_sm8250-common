3255d5b881c3c17e47eb226d20cf8a1497a20026
Roman Kiryanov <rkir@google.com>
2022-08-30 17:18:22 -0700

    sm8250-common: Switch sensors multihal to AIDL
    Bug: 205154779
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: I13c7bb690f3d66e164d0caed0af6bfcf6769ce91

--- common.mk
+++ common.mk
@@ -404,7 +404,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
 
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal
+    android.hardware.sensors-service.multihal
 
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
