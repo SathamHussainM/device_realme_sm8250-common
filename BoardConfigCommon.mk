dc87f6a91a3bb2b91edd3a4f98dbe1d4c25f4ca2
SathamHussainM <sathamhussain.m11@gmail.com>
2022-10-20 10:18:34 +0000

    sm8250-common: Remove SEpolicy neverallow flag
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -207,7 +207,6 @@ ENABLE_VENDOR_RIL_SERVICE := true
 VENDOR_SECURITY_PATCH := $(PLATFORM_SECURITY_PATCH)
 
 # SELinux
-SELINUX_IGNORE_NEVERALLOWS := true
 include device/qcom/sepolicy_vndr-legacy-um/SEPolicy.mk
 include hardware/oplus/sepolicy/qti/SEPolicy.mk
 
