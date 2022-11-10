4277e33c66d4ee14af3bee2cde9a574c496f6611
cristianvaz <cristianvaz81993@gmail.com>
2022-11-10 17:59:24 +0100

    sm8250-common: Add lineage touch support

--- common.mk
+++ common.mk
@@ -455,6 +455,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Touch
+PRODUCT_PACKAGES += \
+    vendor.lineage.touch@1.0-service.oplus
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
