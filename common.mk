2947189c482e165d60d869586e8ddf1493f14b2b
Mashopy <eliasgheeraert@gmail.com>
2022-11-26 23:34:06 +0100

    sm8250-common: Stop building Oplus light service

--- common.mk
+++ common.mk
@@ -275,10 +275,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# Light
-PRODUCT_PACKAGES += \
-    android.hardware.light-service.oplus
-
 # Media
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_audio.xml \
