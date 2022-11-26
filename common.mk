bc0b10744a375c6061f2b9a8c2f261dc19d68c5b
Mashopy <eliasgheeraert@gmail.com>
2022-11-26 23:37:11 +0100

    sm8250-common: Stop inheriting Oplus QSSI overlays

--- common.mk
+++ common.mk
@@ -352,8 +352,6 @@ PRODUCT_PACKAGES += \
     libstagefrighthw
 
 # Overlays
-$(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
-
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay-aosp
 
