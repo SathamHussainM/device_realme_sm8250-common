af5d87857a4dc134d78c6e4c87f0afdcae437317
Vaisakh Murali <mvaisakh@statixos.com>
2023-03-04 23:47:50 +0600

    sm8250-common: Introduce a stub kona-post-boot script
    This will be used to tune some parameters in the device. Only run
    this script after proprietary kernel post-boot has run, so that
    these changes are not overriden by the same.
    
    Change-Id: Ibcef27a2813870c62d54c9d762eae9e03da677ad
    Signed-off-by: Vaisakh Murali <mvaisakh@statixos.com>
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- common.mk
+++ common.mk
@@ -274,6 +274,7 @@ PRODUCT_PACKAGES += \
     fstab.qcom \
     fstab.qcom.ramdisk \
     init.class_main.sh \
+    init.kona.post_boot.sh \
     init.oplus.rc \
     init.oplus.sh \
     init.oplus.touch.rc \
