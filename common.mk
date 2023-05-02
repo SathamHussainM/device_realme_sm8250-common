357383f13f06608160706f46784c79177999a43d
SathamHussainM <sathamhussain.m11@gmail.com>
2023-05-02 19:08:36 +0000

    sm8250-common: Drop overlay-lineage
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- common.mk
+++ common.mk
@@ -346,9 +346,6 @@ PRODUCT_PACKAGES += \
 # Overlays
 $(call inherit-product, hardware/oplus/overlay/qssi/qssi.mk)
 
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay-lineage
-
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     FrameworksResTarget \
