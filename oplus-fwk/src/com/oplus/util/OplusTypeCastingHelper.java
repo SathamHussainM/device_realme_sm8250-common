1b3a2313edd66cdc985d174793b6e7ca420a1edf
Andrea055 <vivanco11.ac@gmail.com>
2023-06-17 12:40:09 +0200

    sm8250-common: Add typeCasting to oplus-fwb Oplus camera stop complain about missing method Partially reversed from OplusCamera.apk using jadx

--- oplus-fwk/src/com/oplus/util/OplusTypeCastingHelper.java
+++ oplus-fwk/src/com/oplus/util/OplusTypeCastingHelper.java
@@ -1,6 +1,11 @@
 package com.oplus.util;
 
+import android.content.res.OplusBaseConfiguration;
+
 public class OplusTypeCastingHelper {
     
     public OplusTypeCastingHelper() {}
+    public static OplusBaseConfiguration typeCasting(Class<OplusBaseConfiguration> configuration, Object context) {
+        return (OplusBaseConfiguration)context;
+    }
 }
