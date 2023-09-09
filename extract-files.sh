4377f97b1502a03123937a8bbf1a7edb9a40da30
Mashopy <eliasgheeraert@gmail.com>
2023-09-10 00:59:33 +0200

    sm8250-common: Update the no-op address of configureRpcThreadpool
    It been changed from AB 0B 00 94 to AF 0B 00 94 during RMX3370_13.1.0.504(EX01) blobs update
    
    Change-Id: I8aa084716b1c29029b7fc8a03eec9ce134205228

--- extract-files.sh
+++ extract-files.sh
@@ -73,7 +73,7 @@ function blob_fixup() {
             grep -q libcamera_metadata_shim.so "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
-            "${SIGSCAN}" -p "AB 0B 00 94" -P "1F 20 03 D5" -f "${2}"
+            "${SIGSCAN}" -p "AF 0B 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
     esac
 }
