de2ee475fb2bbfc51f37fe72428c122488bda045
SathamHussainM <sathamhussain.m11@gmail.com>
2023-10-13 12:48:42 +0000

    [TMP]Revert "sm8250-common: Add support for OZO Audio"
    This reverts commit 4e10dfc009423688d738d6c590204722fa8722bb.
    Signed-off-by: SathamHussainM <sathamhussain.m11@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -104,9 +104,7 @@ DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     $(COMMON_PATH)/device_framework_matrix.xml \
     $(COMMON_PATH)/vendor_framework_compatibility_matrix.xml
 DEVICE_MATRIX_FILE += $(COMMON_PATH)/compatibility_matrix.xml
-DEVICE_MANIFEST_FILE += \
-    $(COMMON_PATH)/manifest.xml \
-    $(COMMON_PATH)/configs/vintf/c2_manifest_vendor.xml
+DEVICE_MANIFEST_FILE += $(COMMON_PATH)/manifest.xml
 
 # Init
 TARGET_INIT_VENDOR_LIB := //$(COMMON_PATH):init_realme_kona
